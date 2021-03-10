7b8793310f629ba40c53ceab21a47f8aad48c9c3
McKynlee Westman <mckynlee.westman@gmail.com>
2021-03-10 15:38:02 -0600

    Clearance level matching secrecy level displayed

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -9,8 +9,32 @@ router.get('/', rejectUnauthenticated, (req, res) => {
   // what is the value of req.user????
   console.log('req.user:', req.user);
 
+  // identify each user's clearance level:
+  let userClearanceLevel = req.user.clearance_level;
+
+  // create variable so we can conditionally set sqlText
+  let sqlText = `SELECT * FROM "secret"
+                 WHERE "secrecy_level" <= $1;`
+
+  // Alternatively:
+  // if (userClearanceLevel < 3) {
+  //   sqlText = `SELECT * FROM "secret"
+  //   WHERE "secrecy_level" < 3;`
+  // }
+  // else if (userClearanceLevel < 6) {
+  //   sqlText = `SELECT * FROM "secret"
+  //   WHERE "secrecy_level" < 6;`
+  // }
+  // else if (userClearanceLevel <= 12) {
+  //   sqlText = `SELECT * FROM "secret"
+  //   WHERE "secrecy_level" <= 12;`
+  // }
+  // else {
+  //   sqlText = `SELECT * FROM "secret";`
+  // }
+
   pool
-    .query(`SELECT * FROM "secret";`)
+    .query(sqlText, [userClearanceLevel])
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
